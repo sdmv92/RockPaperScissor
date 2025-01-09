8d83389271f19959ef473fcf0dc1f8f501113f1a
sdmv <74918482+sdmv92@users.noreply.github.com>
2025-01-09 22:59:30 +0530

    Create Rock, Paper, Scissor Game with HTML display
    Change from console based game to DOM based game.

--- javascript.js
+++ javascript.js
@@ -10,48 +10,57 @@ function getComputerChoice(){
     return aiRandomChoice;
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt ("Rock, Paper, or Scissor??");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
-}
-
 let humanScore=0;
 let computerScore=0;
 function playRound(humanChoice,aiRandomChoice){
-    if (humanChoice===aiRandomChoice){
-        console.log("It's a tie");
+    if(humanScore ==5){
+        result.textContent= "You Won the game!!!"
+        humanScore =0
+        computerScore =0
+    }else if(computerScore ==5){
+        result.textContent= "You lost the game :("
+        humanScore =0
+        computerScore =0
+    }else if (humanChoice===aiRandomChoice){
+
+        result.textContent= "It's a tie"
     }else if (humanChoice==="rock" && aiRandomChoice==="paper"){
-        console.log("You loose. Paper beats Rock");
+        
+        result.textContent="You loose. Paper beats Rock"
         ++computerScore;
     }else if (humanChoice==="rock" && aiRandomChoice==="scissor"){
-        console.log("You win!!. Rock beats Scissor");
+        
+        result.textContent="You win!!. Rock beats Scissor"
         ++humanScore;
     }else if (humanChoice==="paper" && aiRandomChoice==="rock"){
-        console.log("You win!!. Paper beats rock");
+        
+        result.textContent="You win!!. Paper beats rock"
         ++humanScore;
     }else if (humanChoice==="paper" && aiRandomChoice==="scissor"){
-        console.log("You loose. Scissor beats Paper");
+        
+        result.textContent="You loose. Scissor beats Paper"
         ++computerScore;
     }else if (humanChoice==="scissor" && aiRandomChoice==="rock"){
-        console.log("You loose. Rock beats scissor");
+        
+        result.textContent="You loose. Rock beats scissor"
         ++computerScore;
     }else if (humanChoice==="scissor" && aiRandomChoice==="paper"){
-        console.log("You win!!. Scissor beats Paper");
+        
+        result.textContent="You win!!. Scissor beats Paper"
         ++humanScore;
     }
     console.log("Your score: ", humanScore, "Computer score: ", computerScore);
+    score.innerHTML=`Your score: <span style="color:blue ; font-weight:bold ; font-size:24px">${humanScore}</span> || Computer score: <span style="color:red ; font-weight:bold ; font-size:24px">${computerScore}</span>`
 }
 
 
 function playgame(){
-    let roundCount=0
-    while(roundCount<5) {
-    const humanSelection=getHumanChoice();
+    if(humanScore !=5 || computerScore!=5){
+        const humanSelection=getHumanChoice();
     const aiSelection=getComputerChoice().toString();
     playRound(humanSelection,aiSelection);
     roundCount++;
-    }
+    
     if (humanScore>computerScore){
         console.log("You won the game!!!");
     }else if (humanScore<computerScore){
@@ -59,6 +68,16 @@ function playgame(){
     }else {
         console.log("The scores are tied")
     }
+    }
+    
 }
 
-playgame();
+const forRock= document.querySelector("#forRock")
+const forPaper= document.querySelector("#forPaper")
+const forScissor= document.querySelector("#forScissor")
+const result= document.querySelector("#result")
+const score= document.querySelector("#score")
+
+forRock.addEventListener("click",()=>playRound("rock",getComputerChoice().toString()))
+forPaper.addEventListener("click",()=>playRound("paper",getComputerChoice().toString()))
+forScissor.addEventListener("click",()=>playRound("scissor",getComputerChoice().toString()))
